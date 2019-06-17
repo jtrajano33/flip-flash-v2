209cc7175a23a68cb1086903a78a70f00817e019
Justin Trajano <justintra@cloudstaff.com>
2019-06-17 11:52:17 +0800

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const [selectedActive, setSelectedActive] = useState('HomePage')
+
+    const handlePage = page => {
+        setSelectedActive(page)
+    }
+
+    const homePageActive = selectedActive === 'HomePage'? "nav-link active" : "nav-link text-white";
+    const hiraganaActive = selectedActive === 'Hiragana'? "nav-link active" : "nav-link text-white";
+    const katakanaActive = selectedActive === 'Katakana'? "nav-link active" : "nav-link text-white";
+    const kanjiActive = selectedActive === 'Kanji'? "nav-link active" : "nav-link text-white";
+    const contactActive = selectedActive === 'Contact'? "nav-link active" : "nav-link text-white";
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark color m-auto">
@@ -14,24 +26,24 @@ const Navbar = () => {
 
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link active" to="/"> Home </Link>
+                    <li className="nav-item" onClick={() => handlePage('HomePage')}>
+                        <Link className={homePageActive} to="/"> Home </Link>
                     </li>
 
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/hiragana"> Hiragana </Link>
+                    <li className="nav-item" onClick={() => handlePage('Hiragana')}>
+                        <Link className={hiraganaActive} to="/hiragana"> Hiragana </Link>
                     </li>
 
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/katakana"> Katakana </Link>
+                    <li className="nav-item" onClick={() => handlePage('Katakana')}>
+                        <Link className={katakanaActive} to="/katakana"> Katakana </Link>
                     </li>
 
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/kanji"> Kanji </Link>
+                    <li className="nav-item" onClick={() => handlePage('Kanji')}>
+                        <Link className={kanjiActive} to="/kanji"> Kanji </Link>
                     </li>
 
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to="/contact">Contact Us</Link>
+                    <li className="nav-item" onClick={() => handlePage('Contact')}>
+                        <Link className={contactActive} to="/contact">Contact Us</Link>
                     </li>
 
                 </ul>
